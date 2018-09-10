d743b2a1500b74a2e5a1332236f0199c2a98f05d
picazoandre-louis <42677897+picazoandre-louis@users.noreply.github.com>
2018-09-10 08:38:02 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("l(a\n\nle\naf\nfa\n\nll\n\ns)\none\nl\n\niness");
+    }
+}
+//l(a
+//
+//le
+//af
+//fa
+//
+//ll
+//
+//s)
+//one
+//l
+//
+//iness
